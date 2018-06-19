04deeae50b640cacca07bb961ff605c94188e196
chimake <dev.chimaobi@gmail.com>
2018-06-19 09:32:36 +0100

    edit form change

--- application/views/admin/user/edit.php
+++ application/views/admin/user/edit.php
@@ -2,17 +2,7 @@
     <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="card">
             <div class="header">
-                <h2><strong>Vertical</strong> Layout</h2>
-                <ul class="header-dropdown">
-                    <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="zmdi zmdi-more"></i> </a>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a href="javascript:void(0);">Action</a></li>
-                            <li><a href="javascript:void(0);">Another action</a></li>
-                            <li><a href="javascript:void(0);">Something else</a></li>
-                            <li><a href="javascript:void(0);" class="boxs-close">Delete</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <h2><strong>Edit</strong> User</h2>
             </div>
             <div class="body">
                 <form>
@@ -24,13 +14,7 @@
                     <div class="form-group">
                         <input type="password" id="password" class="form-control" placeholder="Enter your password">
                     </div>
-                    <div class="checkbox">
-                        <input id="remember_me" type="checkbox">
-                        <label for="remember_me">
-                            Remember Me
-                        </label>
-                    </div>
-                    <button type="button" class="btn btn-raised btn-primary btn-round waves-effect">LOGIN</button>
+                    <button type="button" class="btn btn-raised btn-primary btn-round waves-effect">Save Details</button>
                 </form>
             </div>
         </div>
